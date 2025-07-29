45cc5585fd09afddecc020d99d3db40e32b7cab5
raaj2045 <raaj2045@gmail.com>
2025-07-29 11:02:52 +0530

    setup database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-const Page = () => {
-  return (<div>Hello world</div>)
+import { prisma } from "@/lib/db";
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  
+  return (<div>{JSON.stringify(users, null, 2)}</div>)
 }
 
 export default Page;

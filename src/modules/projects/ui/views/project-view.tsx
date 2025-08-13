b19d59839b0919c43bc40d5debb6d70b8e9b351b
raaj2045 <mishra_raaj@apple.com>
2025-08-13 11:33:13 +0530

    add code view

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { Suspense, useState } from "react";
+import Link from "next/link";
+import { CodeIcon, CrownIcon, EyeIcon } from "lucide-react";
+
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 
-import { ResizablePanel, ResizablePanelGroup, ResizableHandle } from "@/components/ui/resizable";
-import { MessagesContainer } from "../components/messages-container";
 import { Fragment } from "@/generated/prisma";
-import { ProjectHeader } from "../components/project-header";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ResizablePanel, ResizablePanelGroup, ResizableHandle } from "@/components/ui/resizable";
+
 import { FragmentWeb } from "../components/fragment-web";
+import { ProjectHeader } from "../components/project-header";
+import { MessagesContainer } from "../components/messages-container";
+import { Button } from "@/components/ui/button";
+import { FileExplorer } from "@/components/file-explorer";
 
 interface Props {
     projectId: string;
@@ -16,6 +23,7 @@ interface Props {
 
 export const ProjectView = ({ projectId }: Props) => {
     const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+    const [tabState, setTabState] = useState<"preview" | "code">("preview");
 
     const trpc = useTRPC();
     const { data: project } = useSuspenseQuery(trpc.projects.getOne.queryOptions({
@@ -48,7 +56,39 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={65}
                     minSize={50}
                 >
-                   {activeFragment && <FragmentWeb data={activeFragment} />} 
+                    <Tabs
+                        className="h-full gap-y-0"
+                        defaultValue="preview"
+                        value={tabState}
+                        onValueChange={(value) => setTabState(value as "preview" | "code")}
+                    >
+                        <div className="w-full flex items-center p-2 border-b gap-x-2">
+                            <TabsList className="h-8 p-0 border rounded-md">
+                                <TabsTrigger value="preview" className="rounded-md">
+                                    <EyeIcon /><span>Demo</span>
+                                </TabsTrigger>
+
+                                <TabsTrigger value="code" className="rounded-md">
+                                    <CodeIcon /><span>Code</span>
+                                </TabsTrigger>
+                            </TabsList>
+                            <div className="ml-auto flex items-center gap-x-2">
+                                <Button asChild size="sm" variant="default">
+                                    <Link href="/pricing">
+                                        <CrownIcon /> Upgrade
+                                    </Link>
+                                </Button>
+                            </div>
+                        </div>
+                        <TabsContent value="preview">
+                            {!!activeFragment && <FragmentWeb data={activeFragment} />}
+                        </TabsContent>
+
+                        <TabsContent value="code" className="min-h-0">
+                            {!!activeFragment?.files && <FileExplorer files={activeFragment.files as { [path: string]: string }} />}
+                        </TabsContent>
+                    </Tabs>
+
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
